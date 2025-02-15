427dddda5affab6a768ae01b66ce7931aa7f5416
Lauren Farrell <150389282+BUFFALOLO@users.noreply.github.com>
2025-02-15 15:17:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
 // TASK 1: Adding and Removing Elements
 let fruits = ['apple', 'banana', 'orange'];
-fruits.push('grape'); // Adding a new fruit
-fruits.pop(); // Removing the last fruit
+fruits.push('grape'); 
+fruits.pop(); 
 
 // TASK 2: Sorting Arrays
 let numbers = [3, 1, 5, 2, 4];
-numbers.sort((a, b) => a - b); // Sorting in ascending order
+numbers.sort((a, b) => a - b); 
 
 // TASK 3: Applying Array Methods
 let moreNumbers = [3, 1, 5, 2, 4];
-let doubled = moreNumbers.map(num => num * 2); // Doubling each number
-let filtered = moreNumbers.filter(num => num % 2 !== 0); // Filtering out even numbers
-let sum = moreNumbers.reduce((acc, num) => acc + num, 0); // Calculating the sum
+let doubled = moreNumbers.map(num => num * 2); 
+let filtered = moreNumbers.filter(num => num % 2 !== 0); 
+let sum = moreNumbers.reduce((acc, num) => acc + num, 0); 
 
 console.log('Fruits:', fruits);
 console.log('Sorted Numbers:', numbers);
@@ -29,17 +29,17 @@ console.log('Sum of Numbers:', sum);
 
 // Task 1: Obtaining String Length
 let message = "Hello, World!";
-console.log('String Length:', message.length); // Expected Outcome: Students should be able to understand how to determine the length of a string in JavaScript.
+console.log('String Length:', message.length); 
 
 // Task 2: Converting Cases
 let text = "Hello, World!";
-console.log('Uppercase:', text.toUpperCase()); // Expected Outcome: Students should be able to demonstrate the ability to convert strings between uppercase and lowercase in JavaScript.
+console.log('Uppercase:', text.toUpperCase()); 
 console.log('Lowercase:', text.toLowerCase());
 
 // Task 3: Extracting Substrings
 let sentence = "The quick brown fox jumps over the lazy dog";
-console.log('Substring:', sentence.substring(10, 19)); // Expected Outcome: Students should be able to extract substrings from a given string using the `substring` method in JavaScript.
+console.log('Substring:', sentence.substring(10, 19)); 
 
 // Task 4: Splitting Strings
 let words = "The quick brown fox";
-console.log('Split Words:', words.split(' ')); // Expected Outcome: Students should be able to split a string into an array of substrings using the `split` method in JavaScript.
+console.log('Split Words:', words.split(' ')); 
